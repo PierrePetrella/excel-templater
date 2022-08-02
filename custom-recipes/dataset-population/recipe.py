7f78cf07da18fdb5ba2e8c90790e98fb50d08325
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-02 16:50:36 +0100

    Edited files

--- custom-recipes/dataset-population/recipe.py
+++ custom-recipes/dataset-population/recipe.py
@@ -26,20 +26,33 @@ output_file_name = get_recipe_config().get('output_file_name',input_folder.list_
 
 row_max = 50
 col_max = 50
-# Get handle on wb
-wb = read_wb_from_managed_folder(input_folder)
-# For each row of the metadata Table
-for index, row in input_metadata_df.iterrows():
-    sheet_name = row["sheet_name"]
-    dataset_name = row["dataset_name"]
-    dataset = dataiku.Dataset(dataset_name)
-    df = dataset.get_dataframe()
-    if sheet_name in wb.sheetnames:
-        ws = wb[sheet_name]
-        tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
-        for tag in tags:
-            populate_table_in_ws(df, ws, tag[1], tag[2])
-    else:
-        raise Exception("Sheet {} doesn't exist".format(sheet_name))
 
+### Look for insert_tags and insert the associated dataset ###
+wb = read_wb_from_managed_folder(input_folder)
+
+for sheet_name in wb.sheetnames:
+    ws = wb[sheet_name]
+    tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
+    for tag in tags:
+        dataset_name = tag[0].split(".")[1]
+        dataset = dataiku.Dataset(dataset_name)
+        df = dataset.get_dataframe()
+        populate_table_in_ws(df, ws, tag[1], tag[2])
 write_wb_to_managed_folder(wb,output_folder, output_file_name + ".xlsx")
+
+### OLD - method using the metadata mapping dataset ###
+#wb = read_wb_from_managed_folder(input_folder)
+## For each row of the metadata Table
+#for index, row in input_metadata_df.iterrows():
+#    sheet_name = row["sheet_name"]
+#    dataset_name = row["dataset_name"]
+#    dataset = dataiku.Dataset(dataset_name)
+#    df = dataset.get_dataframe()
+#    if sheet_name in wb.sheetnames:
+#        ws = wb[sheet_name]
+#        tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
+#        for tag in tags:
+#            populate_table_in_ws(df, ws, tag[1], tag[2])
+#    else:
+#        raise Exception("Sheet {} doesn't exist".format(sheet_name))
+#write_wb_to_managed_folder(wb,output_folder, output_file_name + ".xlsx")
