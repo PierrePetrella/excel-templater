1d8c5e458fb88b3f52b93f7cd325d42aa1bc9a91
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-02 17:02:26 +0100

    Edited files

--- custom-recipes/dataset-population/recipe.py
+++ custom-recipes/dataset-population/recipe.py
@@ -31,7 +31,9 @@ for sheet_name in wb.sheetnames:
     tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
     for tag in tags:
         if len(tag)!=3:
-            raise Exception("Tag {} is not well formatted".format(tag))
+            raise Exception("Tag cannot be properly extracted from sheet {}".format(sheet_name))
+        if not tag[0].contains("."):
+            raise Exception("Tag {} is not well formatted".format(tag[0]))
         dataset_name = tag[0].split(".")[1]
         dataset = dataiku.Dataset(dataset_name)
         df = dataset.get_dataframe()
