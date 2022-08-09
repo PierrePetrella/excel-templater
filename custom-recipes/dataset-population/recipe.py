2aba7fea64c7c55164c34979a758fc4dd9043b35
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-09 09:20:27 +0100

    Edited files

--- custom-recipes/dataset-population/recipe.py
+++ custom-recipes/dataset-population/recipe.py
@@ -25,20 +25,6 @@ col_max = 50
 
 ### Look for insert_tags and insert the associated dataset ###
 wb = read_wb_from_managed_folder(input_folder)
-
-def popluate_wb_from_dataset(wb, insert_tag, row_max=50, col_max=50):
-    for sheet_name in wb.sheetnames:
-        ws = wb[sheet_name]
-        tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
-        for tag in tags:
-            if len(tag)!=3:
-                raise Exception("Tag cannot be properly extracted from sheet {}".format(sheet_name))
-            if "." not in tag[0]:
-                raise Exception("Tag {} is not well formatted".format(tag[0]))
-            dataset_name = tag[0].split(".")[1]
-            dataset = dataiku.Dataset(dataset_name)
-            df = dataset.get_dataframe()
-            populate_table_in_ws(df, ws, tag[1], tag[2])
         
 popluate_wb_from_dataset(wb, insert_tag, row_max, col_max)
 
--- python-lib/helper_functions.py
+++ python-lib/helper_functions.py
@@ -86,3 +86,17 @@ def poluate_wb_from_sql(wb,cnx_name,sql_tag,row_max=50, col_max=50):
             query = insert_project_variables_in_query(query)
             df = get_df_from_query(cnx_name,query)
             ws = populate_table_in_ws(df, ws,tag[1], tag[2])
+            
+def popluate_wb_from_dataset(wb, insert_tag, row_max=50, col_max=50):
+    for sheet_name in wb.sheetnames:
+        ws = wb[sheet_name]
+        tags = find_tags_in_ws(ws, insert_tag, row_max, col_max)
+        for tag in tags:
+            if len(tag)!=3:
+                raise Exception("Tag cannot be properly extracted from sheet {}".format(sheet_name))
+            if "." not in tag[0]:
+                raise Exception("Tag {} is not well formatted".format(tag[0]))
+            dataset_name = tag[0].split(".")[1]
+            dataset = dataiku.Dataset(dataset_name)
+            df = dataset.get_dataframe()
+            populate_table_in_ws(df, ws, tag[1], tag[2])
