8a5020383043b254ea0620944599f75930ff94f0
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-09 09:12:28 +0100

    Edited files

--- custom-recipes/sql-table-population/recipe.py
+++ custom-recipes/sql-table-population/recipe.py
@@ -14,7 +14,7 @@ output_folder_name = get_output_names_for_role('output_folder')[0]
 output_folder = dataiku.Folder(output_folder_name)
 
 # Retrieve mandatory user-defined parameters
-query_tag = get_recipe_config().get('SQL_Tag', "SQL_Tag")
+sql_tag = get_recipe_config().get('SQL_Tag', "SQL_Tag")
 cnx_name = get_recipe_config().get('sql_cnx', "")
 output_file_name = get_recipe_config().get('output_file_name',input_folder.list_paths_in_partition()[0].split(".")[0] )
 
@@ -24,15 +24,18 @@ col_max = 50
 # Get haddle on the input workbook
 wb = read_wb_from_managed_folder(input_folder)
 
-for sheet_name in wb.sheetnames:
-    ws = wb[sheet_name]
-    tags = find_tags_in_ws(ws,query_tag, row_max, col_max)
-    for tag in tags:
-        raw_query = tag[0]
-        query = raw_query.replace(query_tag,"")
-        query = insert_project_variables_in_query(query)
-        df = get_df_from_query(cnx_name,query)
-        ws = populate_table_in_ws(df, ws,tag[1], tag[2])
-        
+def poluate_wb_from_sql(wb,cnx_name,sql_tag,row_max=50, col_max=50):
+    for sheet_name in wb.sheetnames:
+        ws = wb[sheet_name]
+        tags = find_tags_in_ws(ws,sql_tag, row_max, col_max)
+        for tag in tags:
+            raw_query = tag[0]
+            query = raw_query.replace(sql_tag,"")
+            query = insert_project_variables_in_query(query)
+            df = get_df_from_query(cnx_name,query)
+            ws = populate_table_in_ws(df, ws,tag[1], tag[2])
+
+poluate_wb_from_sql(wb,cnx_name,sql_tag,row_max=50, col_max=50)
+  
 write_wb_to_managed_folder(wb,output_folder, output_file_name + ".xlsx")
 
