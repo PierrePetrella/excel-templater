8f26e7a369c10e39e889cc9fd26d98c8bcd27e3f
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-09 09:35:35 +0100

    Edited files

--- custom-recipes/dataset-population/recipe.py
+++ custom-recipes/dataset-population/recipe.py
@@ -17,7 +17,7 @@ output_folder_name = get_output_names_for_role('output_folder')[0]
 output_folder = dataiku.Folder(output_folder_name)
 
 # Retrieve mandatory user-defined parameters
-insert_tag = get_recipe_config().get('input_tag', "DATASET")
+dataset_tag = get_recipe_config().get('dataset_tag', "DATASET")
 output_file_name = get_recipe_config().get('output_file_name',input_folder.list_paths_in_partition()[0].split(".")[0] )
 
 row_max = 50
@@ -26,6 +26,6 @@ col_max = 50
 ### Look for insert_tags and insert the associated dataset ###
 wb = read_wb_from_managed_folder(input_folder)
         
-popluate_wb_from_dataset(wb, insert_tag, row_max, col_max)
+popluate_wb_from_dataset(wb, dataset_tag, row_max, col_max)
 
 write_wb_to_managed_folder(wb,output_folder, output_file_name + ".xlsx")
