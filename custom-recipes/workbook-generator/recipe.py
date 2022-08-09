610fe3b8bf43e8d76deabd4ddcbe9a667710c9d7
pierrepetrella <pierre.petrella@dataiku.com>
2022-08-09 09:39:56 +0100

    Edited file 'custom-recipes/workbook-generator/recipe.py'

--- custom-recipes/workbook-generator/recipe.py
+++ custom-recipes/workbook-generator/recipe.py
@@ -18,7 +18,7 @@ output_folder_name = get_output_names_for_role('output_folder')[0]
 output_folder = dataiku.Folder(output_folder_name)
 
 # Retrieve mandatory user-defined parameters
-sql_tag = get_recipe_config().get('sql_Tag', "SQL_Tag")
+sql_tag = get_recipe_config().get('sql_tag', "SQL_Tag")
 cnx_name = get_recipe_config().get('sql_cnx', "")
 
 dataset_tag = get_recipe_config().get('dataset_tag', "DATASET")
